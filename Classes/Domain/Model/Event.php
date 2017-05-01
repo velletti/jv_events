8c08814d1246ca83af059c7e385d70644ef490a5
Joerg Velletti <jvelletti@gmail.com>
2017-05-01 11:31:33 +0200

    [register] - Show Free Seats Warning

--- Classes/Domain/Model/Event.php
+++ Classes/Domain/Model/Event.php
@@ -1670,6 +1670,30 @@ class Event extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
         return true ;
     }
 
+    /** + +++ special Helper functions for the model  */
+
+    /**
+     * @return boolean
+     */
+    public function isNoFreeSeats()
+    {
+
+        // only Check if is with internal registration and we have free seats
+        if ($this->withRegistration ) {
+            // Internal Registration Process : check $this->availableSeats  Seats against Registered
+            if (($this->registeredSeats + $this->unconfirmedSeats +1) > ($this->availableSeats + $this->availableWaitingSeats)  ) {
+                //  echo "<br>Line: " . __LINE__ . " : " . " File: " . __FILE__ . '<br>$this->registeredSeats + $this->unconfirmedSeats +1) > ($this->availableSeats + $this->availableWaitingSeat <hr>';
+                    return TRUE;
+            }
+            if (($this->unconfirmedSeats + 1) > ($this->availableSeats) && ( $this->availableSeats > 0 )) {
+                //  echo "<br>Line: " . __LINE__ . " : " . " File: " . __FILE__ . '<br>$this->unconfirmedSeats + 1) > ($this->availableSeats) && ( $this->availableSeats > 0  <hr>';
+                return TRUE;
+            }
+        }
+        // ok: we have free seats or it is eyternal registration so we do not need or can not show the No Free Seats warning
+        return false ;
+    }
+
 	/**
 	 * @return boolean
 	 */
