26e7cbdd2afd85bc5067452dd578a20c47f3c995
velletti <jvelletti@allplan.com>
2017-05-02 13:22:13 +0200

    [#144687375] - Do Not Show All Languages Option and Show Localized Tags/Categories

--- Classes/UserFunc/Flexforms.php
+++ Classes/UserFunc/Flexforms.php
@@ -46,4 +46,38 @@ class Flexforms {
 
 		return $formField;
 	}
+
+	/** used in event Model TCA to show the Translations of Tags and Categories, but still work with the uid of the default record
+     * */
+	public function TranslateMMvalues($config) {
+	    if( $config['row']['sys_language_uid'][0] < 1 ) {
+	        return $config ;
+        }
+
+        // Works only Out Of the Box if label Field and Foreign_table in TCA is set AND  only ONE Field / Table Name is set and not splitted, joined  with a komma
+        $table = $config['config']['foreign_table'] ;
+
+        $nameField = $GLOBALS['TCA'][$table]['ctrl']['label'] ;
+
+        if( $table . $nameField == '' || strpos( $table . $nameField  , ",") > 0 ) {
+            return $config ;
+        }
+
+        /**
+         * @var $db \TYPO3\CMS\Core\Database\DatabaseConnection
+         */
+        $db = $GLOBALS['TYPO3_DB'];
+        $rows = $db->exec_SELECTgetRows('uid,l10n_parent,' . $nameField ,  $table , 'sys_language_uid=' . $config['row']['sys_language_uid'][0]
+            , '',  '', '', $uidIndexField = 'l10n_parent') ;
+
+        foreach ( $config['items'] as $key => $item ) {
+            $uid = $item[1] ;
+            if( is_array(  $rows[$uid] ) ) {
+                $config['items'][$key][0] = $config['items'][$key][0] . " (" . $rows[$uid][ $nameField ] . ")" ;
+            }
+            $config['items'][$key][0] = "[" . $config['items'][$key][0] = $config['items'][$key][1] . "] ". $config['items'][$key][0] ;
+        }
+
+        return $config ;
+    }
 }
