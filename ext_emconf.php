0399ba7dfec8a241a493858983cb141ca05f28d6
velletti <jvelletti@allplan.com>
2017-12-19 14:12:09 +0100

    [CHORE] Removing XMLns:f= ... from all templates

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.1.6',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.7.0-7.6.99',
+			'typo3' => '8.7.0-8.7.99',
 
 		),
 		'conflicts' => array(
