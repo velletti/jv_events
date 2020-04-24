77704374557a9b7acbd6139553b19130b00bfca3
Joerg Velletti <jvelletti@gmail.com>
2020-04-24 19:45:07 +0200

    [LTS 9] wartungsarbeiten und finale Änderungen

--- Classes/UserFunc/Flexforms.php
+++ Classes/UserFunc/Flexforms.php
@@ -8,6 +8,11 @@
 
 namespace JVE\JvEvents\UserFunc;
 
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 class Flexforms {
 	public function getSettings() {
 		$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
@@ -47,37 +52,52 @@ class Flexforms {
 		return $formField;
 	}
 
-	/** used in event Model TCA to show the Translations of Tags and Categories, but still work with the uid of the default record
+    /** used in event Model TCA to show the Translations of Tags and Categories, but still work with the uid of the default record
+     * does not work in 9.5.15 - 20.4.2020 but there is a closed issue / patch
+     * https://forge.typo3.org/issues/85142
      * */
-	public function TranslateMMvalues($config) {
-	    if( $config['row']['sys_language_uid'][0] < 1 ) {
-	        return $config ;
+    public function TranslateMMvalues($config) {
+
+
+        if( ! is_array($config ) || $config['row']['sys_language_uid'][0] < 1  || ! is_array($config['items'])) {
+            return $config ;
         }
 
         // Works only Out Of the Box if label Field and Foreign_table in TCA is set AND  only ONE Field / Table Name is set and not splitted, joined  with a komma
         $table = $config['config']['foreign_table'] ;
 
-        $nameField = $GLOBALS['TCA'][$table]['ctrl']['label'] ;
+        $nameField = trim( $GLOBALS['TCA'][$table]['ctrl']['label'] ) ;
 
         if( $table . $nameField == '' || strpos( $table . $nameField  , ",") > 0 ) {
             return $config ;
         }
 
-        /**
-         * @var $db \TYPO3\CMS\Core\Database\DatabaseConnection
-         */
-        $db = $GLOBALS['TYPO3_DB'];
-        $rows = $db->exec_SELECTgetRows('uid,l10n_parent,' . $nameField ,  $table , 'sys_language_uid=' . $config['row']['sys_language_uid'][0]
-            , '',  '', '', $uidIndexField = 'l10n_parent') ;
+
 
         foreach ( $config['items'] as $key => $item ) {
             $uid = $item[1] ;
-            if( is_array(  $rows[$uid] ) ) {
-                $config['items'][$key][0] = $config['items'][$key][0] . " (" . $rows[$uid][ $nameField ] . ")" ;
+
+            $row = $this->getRow($table , $nameField , $config['row']['sys_language_uid'][0] , $uid ) ;
+            if( is_array(  $row ) ) {
+                $config['items'][$key][0] = $config['items'][$key][0] . " (" . $row[ $nameField ] . ")" ;
             }
-            $config['items'][$key][0] = "[" . $config['items'][$key][0] = $config['items'][$key][1] . "] ". $config['items'][$key][0] ;
+            // $config['items'][$key][0] = "[" . $config['items'][$key][0] = $config['items'][$key][1] . "] ". $config['items'][$key][0] ;
+
         }
 
         return $config ;
+    }
+    private function getRow($table , $nameField , $lngField , $uid ) {
+        /** @var ConnectionPool $connectionPool */
+        $connectionPool = GeneralUtility::makeInstance( "TYPO3\\CMS\\Core\\Database\\ConnectionPool");
+
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = $connectionPool->getConnectionForTable($table)->createQueryBuilder();
+        $queryBuilder->select('l18n_parent','uid',$nameField) ->from($table) ;
+        $expr = $queryBuilder->expr();
+        $queryBuilder->where( $expr->eq('sys_language_uid',
+            $queryBuilder->createNamedParameter( $lngField , Connection::PARAM_INT))
+        ) ;
+        return  $queryBuilder->andWhere($expr->eq('l18n_parent', intval($uid )))->execute()->fetch() ;
     }
 }
