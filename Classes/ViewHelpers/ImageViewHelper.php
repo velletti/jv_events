526805394463361e3c8ae2e91e7d6f169e10e997
Joerg Velletti <jvelletti@gmail.com>
2020-11-15 09:28:23 +0100

    [TANGO] do not throw exception if Organizer Image does not exist
    remove registering arguments as this is all done in parent class already

--- Classes/ViewHelpers/ImageViewHelper.php
+++ Classes/ViewHelpers/ImageViewHelper.php
@@ -45,25 +45,6 @@ class ImageViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\ImageViewHelper
     public function initializeArguments()
     {
         parent::initializeArguments();
-        $this->registerUniversalTagAttributes();
-        $this->registerTagAttribute('alt', 'string', 'Specifies an alternate text for an image', false);
-        $this->registerTagAttribute('ismap', 'string', 'Specifies an image as a server-side image-map. Rarely used. Look at usemap instead', false);
-        $this->registerTagAttribute('longdesc', 'string', 'Specifies the URL to a document that contains a long description of an image', false);
-        $this->registerTagAttribute('usemap', 'string', 'Specifies an image as a client-side image-map', false);
-
-        $this->registerArgument('src', 'string', 'a path to a file, a combined FAL identifier or an uid (int). If $treatIdAsReference is set, the integer is considered the uid of the sys_file_reference record. If you already got a FAL object, consider using the $image parameter instead');
-        $this->registerArgument('treatIdAsReference', 'bool', 'given src argument is a sys_file_reference record');
-        $this->registerArgument('image', 'object', 'a FAL object');
-        $this->registerArgument('crop', 'string|bool', 'overrule cropping of image (setting to FALSE disables the cropping set in FileReference)');
-        $this->registerArgument('cropVariant', 'string', 'select a cropping variant, in case multiple croppings have been specified or stored in FileReference', false, 'default');
-
-        $this->registerArgument('width', 'string', 'width of the image. This can be a numeric value representing the fixed width of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.');
-        $this->registerArgument('height', 'string', 'height of the image. This can be a numeric value representing the fixed height of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.');
-        $this->registerArgument('minWidth', 'int', 'minimum width of the image');
-        $this->registerArgument('minHeight', 'int', 'minimum height of the image');
-        $this->registerArgument('maxWidth', 'int', 'maximum width of the image');
-        $this->registerArgument('maxHeight', 'int', 'maximum height of the image');
-        $this->registerArgument('absolute', 'bool', 'Force absolute URL', false, false);
     }
 
     /**
