5db175ff94cab25f8ca7e47fadf069a8300f7b13
Joerg Velletti <jvelletti@allplan.com>
2022-10-27 09:34:43 +0200

    CHORE  rector TCA - d not use Import of FQCN in V10 in ext_tables at the moment

--- ext_tables.php
+++ ext_tables.php
@@ -1,15 +1,13 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'JvEvents' ,
-	'Events',
-	'Events'
+    'JvEvents' ,
+    'Events',
+    'Events'
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
@@ -34,46 +32,45 @@ if (TYPO3_MODE === 'BE') {
         )
     );
 
-
-/*
-    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
-        'web',
-        'eventmngt',
-        'after:List',
-        '',
-        [
-           'routeTarget' => JVE\JvEvents\Controller\EventBackendController::class . '::listAction',
-            'access' => 'user,group',
-            'name' => 'web_eventmngt',
-            'workspaces' => 'online',
-            'icon' => 'EXT:jv_events/ext_icon_importer.gif',
-            'labels' => [
-                'LLL:EXT:jv_events/Resources/Private/Language/locallang_eventmngt.xlf',
-            ],
-            'navigationFrameModule' => 'web',
-            'navigationFrameModuleParameters' => ['currentModule' => 'web_eventmngt'],
-        ]
-    );
-*/
+    /*
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+            'web',
+            'eventmngt',
+            'after:List',
+            '',
+            [
+               'routeTarget' => JVE\JvEvents\Controller\EventBackendController::class . '::listAction',
+                'access' => 'user,group',
+                'name' => 'web_eventmngt',
+                'workspaces' => 'online',
+                'icon' => 'EXT:jv_events/ext_icon_importer.gif',
+                'labels' => [
+                    'LLL:EXT:jv_events/Resources/Private/Language/locallang_eventmngt.xlf',
+                ],
+                'navigationFrameModule' => 'web',
+                'navigationFrameModuleParameters' => ['currentModule' => 'web_eventmngt'],
+            ]
+        );
+    */
 }
 
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_event', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_event.xlf');
-ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_event');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_event', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_event.xlf');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_event');
 
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_subevent', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_subevent.xlf');
-ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_subevent');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_subevent', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_subevent.xlf');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_subevent');
 
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_organizer', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_organizer.xlf');
-ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_organizer');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_organizer', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_organizer.xlf');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_organizer');
 
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_location', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_location.xlf');
-ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_location');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_location', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_location.xlf');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_location');
 
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_registrant', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_registrant.xlf');
-ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_registrant');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_registrant', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_registrant.xlf');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_registrant');
 
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_category', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_category.xlf');
-ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_category');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_category', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_category.xlf');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_category');
 
-ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_tag', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_tag.xlf');
-ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_tag');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_jvevents_domain_model_tag', 'EXT:jv_events/Resources/Private/Language/locallang_csh_tx_jvevents_domain_model_tag.xlf');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_jvevents_domain_model_tag');
